3b4a4814152ca619db45eef959e3395de9e1dddb
nexepic <remarkable.yh@gmail.com>
2025-02-11 22:05:38 +0800

    desc updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head suppressHydrationWarning={true}>
+        <link rel="icon" href="/favicon.svg" sizes="any"/>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
